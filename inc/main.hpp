75809d78ed4248d8166da6eac7abc188835b57ba
Ashes110 <atsu110121@gmail.com>
2023-07-17 18:43:45 +0900

    WIP

---
+++ inc/main.hpp
@@ -0,0 +1,11 @@
+#ifndef MAIN_HPP
+#define MAIN_HPP
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <iterator>
+
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,18 @@
-int main(int argc, char const *argv[]) {
-	/* code */
-	return 0;
+#include "main.hpp"
+
+int error_log(std::string string)
+{
+	std::ofstream log;
+	log.open("error.log", std::ios::app);
+	log << string << std::endl;
+	log.close();
+	std::cout << string << std::endl;
+	return(1);
+}
+
+int main(int argc, char const *argv[])
+{
+	if (argc > 2)
+		return(error_log("Too many arguments"));
+
 }
