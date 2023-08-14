e52d2e532b3923d3176a3b892453066040dd1881
Ashes110 <atsu110121@gmail.com>
2023-08-14 10:46:08 +0000

    add Error class

---
+++ inc/Error.hpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <stdexcept>
+#include <string>
+
+class GenericException : public std::exception
+{
+  private:
+	int errorCode;
+	std::string errorMessage;
+
+  public:
+	GenericException(int code, const std::string &message);
+	virtual ~GenericException() throw() {}
+	int code() const;
+	const char* what() const throw();
+};
---
+++ src/utils/Error.cpp
@@ -0,0 +1,13 @@
+#include "Error.hpp"
+
+GenericException::GenericException(int code, const std::string &message) : errorCode(code), errorMessage(message) {}
+
+int GenericException::code() const
+{
+	return (errorCode);
+}
+
+const char *GenericException::what() const throw()
+{
+	return (errorMessage.c_str());
+}
