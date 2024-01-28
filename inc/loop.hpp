1943040686bff0280829cb717bf326962dd52203
ilovenoah <ilovenoah0105@gmail.com>
2024-01-28 12:23:51 +0900

    setRevents handle poll fds int loop.cpp

---
+++ inc/loop.hpp
@@ -0,0 +1,11 @@
+#ifndef LOOP_HPP
+#define LOOP_HPP
+
+#define POLLTIMEOUT 
+#include <poll.h>
+#include <map>
+#include <vector>
+#include "ServerSocket.hpp"
+#include "utils.hpp"
+
+#endif
---
+++ src/loop/loop.cpp
@@ -0,0 +1,29 @@
+#include "loop.hpp"
+
+static std::string convertIPAddress(in_addr_t address) {
+    std::ostringstream oss;
+    oss << (address & 0xFF) << '.'               // 最下位のバイト
+        << ((address >> 8) & 0xFF) << '.'        // さらに次のバイト
+        << ((address >> 16) & 0xFF) << '.'       // 次のバイト
+        << ((address >> 24) & 0xFF);             // 最上位のバイト
+    return oss.str();
+}
+
+static bool setRevents(std::map<int, ServerSocket> &ssmap) {
+    std::vector<struct pollfd> pollfds;
+    for(std::map<int, ServerSocket>::iterator iter = ssmap.begin(); iter != ssmap.end(); ++iter) {
+        struct pollfd pfd;
+        std::memset(&pfd, 0, sizeof(struct pollfd));
+        pfd.fd = iter->first;
+        pfd.events = POLLIN | POLLOUT | POLLHUP;
+        pollfds.push_back(pfd);
+    }
+    if (poll(pollfds.data(), pollfds.size(), 0) == -1) {
+        utils::putSysError("poll");
+        return false;
+    }
+    for(std::vector<struct pollfd>::iterator iter = pollfds.begin(); iter != pollfds.end(); ++iter) {
+        ssmap[iter->fd].setRevents(iter->revents);
+    }
+    return true;
+}
