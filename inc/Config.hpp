bff107eff1cf07e156e060bed788a42c18dfc566
taiphyyy <ilovenoah0105@gmail.com>
2024-02-06 14:23:37 +0900

    config class changed

--- inc/Config.hpp
+++ inc/Config.hpp
@@ -1,38 +1,21 @@
 #ifndef CONFIG_HPP
 #define CONFIG_HPP
 
-#include "Server.hpp"
-#include "env.hpp"
-
-#define DEFAULT_CONF "conf/sample.conf"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <sys/stat.h>
 
 class Config {
 	private:
-		std::vector<Server> _servers;
-		std::string _filePath;
-
-		int _bracket_count;
+		std::fstream _file;
+		// std::vector<Server> _servers;
 
 	public:
-		Config();
-		~Config();
-		Config(const Config &copy);
-		Config &operator=(const Config &copy);
-
-		std::ifstream _fileStream;
-
-		void setServers(int argc, const char *argv[]);
-		void setFilePath(std::string file_path);
-
-		const std::vector<Server> &getServers() const;
-		const std::string &getFilePath() const;
-
-		void parseConfig(int argc, const char *argv[]);
-		void readFile();
-		void parseFile();
-		void parseLine(std::string &line);
-		void removeUnwanted(std::string &line);
-		void removeComments(std::string &line);
+		bool open(char const *path);
+		bool close();
+		bool load();
 };
+
 
 #endif
