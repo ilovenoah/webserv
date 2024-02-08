2ee1f0a78b8734fee34d7412d926b0e1be794f0a
taiphyyy <ilovenoah0105@gmail.com>
2024-02-08 16:44:33 +0900

    Concludes the similar lines between Server class and Location class into  AConfigurable

---
+++ inc/AConfigurable.hpp
@@ -0,0 +1,39 @@
+#ifndef CONFIGURABLE_HPP
+#define CONFIGURABLE_HPP
+
+#include <vector>
+#include <string>
+#include <map>
+#include <fstream>
+#include <sstream>
+#include "utils.hpp"
+
+class AConfigurable {
+	private:
+		std::vector<std::string> _allowMethods;
+		bool _autoindex;
+		std::vector<std::string> _index;
+		std::size_t _clientMaxBodySize;
+		std::vector<std::string> _cgi_extensions;
+		std::string _return;
+		std::map<std::string, std::string> _errorPages;
+
+	public:
+		virtual ~AConfigurable() = 0;
+		virtual bool setAllowMethods(std::string const &attribute, std::fstream &file) = 0;
+		virtual const std::vector<std::string> &getAllowMethods() const = 0;
+		virtual bool setAutoIndex(std::string const &attribute, std::fstream &file) = 0;
+		virtual const bool &getAutoindex() const = 0;
+		virtual bool setIndex(std::string const &attribute, std::fstream &file) = 0;
+		virtual const std::vector<std::string> &getIndex() const = 0;
+		virtual bool setClientMaxBodySize(std::string const &attribute, std::fstream &file) = 0;
+		virtual const std::size_t &getClientMaxBodySize() const = 0;
+		virtual bool setCgiExtensions(std::string const &attribute, std::fstream &file) = 0;
+		virtual const std::vector<std::string> &getCgiExtensions() const = 0;
+		virtual bool setReturn(std::string const &attribute, std::fstream &file) = 0;
+		virtual bool setErrorPages(std::string const &attribute, std::fstream &file) = 0;
+		virtual const std::string &getReturn() const = 0;
+		virtual const std::map<std::string, std::string> &getErrorPages() const = 0;
+};
+
+#endif
---
+++ src/conf/AConfigurable.cpp
@@ -0,0 +1,155 @@
+#include "AConfigurable.hpp"
+
+AConfigurable::~AConfigurable() {}
+
+bool AConfigurable::setAllowMethods(std::string const &attribute, std::fstream &file) {
+	(void)file;
+	std::stringstream ss(attribute);
+	std::string elem;
+	ss >> elem;
+	if (ss.eof() == true) { return false; }
+	this->_allowMethods.clear();
+	while (ss.eof() == false) {
+		elem.clear();
+		ss >> elem;
+		if (elem != "GET" && elem != "POST" && elem != "DELETE") { return false; }
+		this->_allowMethods.push_back(elem);
+	}
+	return true;
+}
+
+const std::vector<std::string> &AConfigurable::getAllowMethods() const {
+	return this->_allowMethods;
+}
+bool AConfigurable::setAutoIndex(std::string const &attribute, std::fstream &file) {
+	(void)file;
+	std::stringstream ss(attribute);
+	std::string elem;
+	ss >> elem;
+	if (ss.eof() == true) { return false; }
+	elem.clear();
+	ss >> elem;
+	if (ss.eof() == false) { return false; }
+	if (elem == "on") {
+		this->_autoindex = true;
+	} else if (elem == "off") {
+		this->_autoindex = false;
+	} else {
+		return false;
+	}
+	return true;
+}
+
+const bool &AConfigurable::getAutoindex() const {
+	return this->_autoindex;
+}
+
+bool AConfigurable::setIndex(std::string const &attribute, std::fstream &file) {
+	(void)file;
+	std::stringstream ss(attribute);
+	std::string elem;
+	ss >> elem;
+	if (ss.eof() == true) { return false; }
+	while (ss.eof() == false) {
+		elem.clear();
+		ss >> elem;
+		this->_index.push_back(elem);
+	}
+	return true;
+}
+
+const std::vector<std::string> &AConfigurable::getIndex() const {
+	return this->_index;
+}
+
+bool AConfigurable::setClientMaxBodySize(std::string const &attribute, std::fstream &file) {
+	(void)file;
+	std::stringstream ss(attribute);
+	std::string elem;
+	ss >> elem;
+	if (ss.eof() == true) { return false; }
+	elem.clear();
+	ss >> elem;
+	if (ss.eof() == false) { return false; }
+	this->_clientMaxBodySize = utils::decStrToSizeT(elem);
+	return true;
+}
+
+const std::size_t &AConfigurable::getClientMaxBodySize() const {
+	return this->_clientMaxBodySize;
+}
+
+bool AConfigurable::setCgiExtensions(std::string const &attribute, std::fstream &file) {
+	(void)file;
+	std::stringstream ss(attribute);
+	std::string elem;
+	ss >> elem;
+	if (ss.eof() == true) { return false; }
+	this->_cgi_extensions.clear();
+	while (ss.eof() == false) {
+		elem.clear();
+		ss >> elem;
+		if (elem[0] != '.') { return false; };
+        for (size_t i = 1; i < elem.length(); ++i) {
+            if (!std::isalnum(elem[i])) { return false; }
+        }
+		this->_cgi_extensions.push_back(elem);
+	}
+	return true;
+}
+
+const std::vector<std::string> &AConfigurable::getCgiExtensions() const {
+	return this->_cgi_extensions;
+}
+
+bool AConfigurable::setReturn(std::string const &attribute, std::fstream &file) {
+	(void)file;
+	std::stringstream ss(attribute);
+	std::string elem;
+	ss >> elem;
+	if (ss.eof() == true) { return false; }
+	elem.clear();
+	ss >> elem;
+	if (ss.eof() == false) { return false; }
+	ss.str("");
+	ss.clear();
+	ss << elem;
+	std::string protocol;
+	std::getline(ss, protocol, ':');
+	if (protocol.compare("http") != 0 && protocol.compare("https") != 0) { return false ;}
+	this->_return = elem;
+	return true;
+}
+
+static bool isStatusCode(std::string const &str) {
+	if (str.size() != 3) { return false; }
+	for (std::string::const_iterator iter = str.begin(); iter != str.end(); ++iter) {
+		if (std::isdigit(*iter) == false) { return false; }
+	}
+	return true;
+}
+
+bool AConfigurable::setErrorPages(std::string const &attribute, std::fstream &file) {
+	(void)file;
+	std::stringstream ss(attribute);
+	std::string elem;
+	std::vector<std::string> statusCodes;
+	ss >> elem;
+	if (ss.eof() == true) { return false; }
+	elem.clear();
+	while (ss.eof() == false) {
+		ss >> elem;
+		if (isStatusCode(elem) == false) { break; }
+		statusCodes.push_back(elem);
+	}
+	if (ss.eof() == false) { return false; }
+	if (statusCodes.size() == 0) { return false; }
+	for (std::vector<std::string>::iterator iter = statusCodes.begin(); iter != statusCodes.end(); ++iter) {
+		this->_errorPages.insert(std::pair<std::string, std::string>(*iter, elem));
+	}
+	return true;
+}
+
+const std::string &AConfigurable::getReturn() const {
+	return this->_return;
+}
