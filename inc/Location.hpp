3a1884edc65bb358733dbd685bb5007cfd1210c3
taiphyyy <ilovenoah0105@gmail.com>
2024-02-08 14:58:47 +0900

    Location class holds information of location directive

--- inc/Location.hpp
+++ inc/Location.hpp
@@ -1,12 +1,18 @@
 #ifndef LOCATION_HPP
 #define LOCATION_HPP
 
-#include "Error.hpp"
-#include "env.hpp"
+#include <string>
 
 class Location {
 	private:
-	
+		std::vector<std::string> _allowMethods;
+		bool _autoindex;
+		std::vector<std::string> _index;
+		std::size_t _clientMaxBodySize;
+		std::vector<std::string> _cgi_extensions;
+		std::string _return;
+		std::map<std::string, std::string> _errorPages;
+
 	public:
 };
 
