722aa63e48892936173e914633961378cebc4efe
taiphyyy <ilovenoah0105@gmail.com>
2024-02-08 16:20:13 +0900

    Location set its path with setLocationPath()

--- inc/Location.hpp
+++ inc/Location.hpp
@@ -2,9 +2,12 @@
 #define LOCATION_HPP
 
 #include <string>
+#include <fstream>
+#include <sstream>
 
 class Location {
 	private:
+		std::string _path;
 		std::vector<std::string> _allowMethods;
 		bool _autoindex;
 		std::vector<std::string> _index;
@@ -12,8 +15,10 @@ class Location {
 		std::vector<std::string> _cgi_extensions;
 		std::string _return;
 		std::map<std::string, std::string> _errorPages;
+		std::size_t _lineCount;
 
 	public:
+		bool setLocationPath(std::string const &attribute);
 };
 
 #endif
--- src/conf/Location.cpp
+++ src/conf/Location.cpp
@@ -1,2 +1,18 @@
 #include "Location.hpp"
 
+bool Location::setLocationPath(std::string const &attribute) {
+	std::stringstream ss(attribute);
+	std::string elem;
+
+	ss >> elem;
+	if (ss.eof() == true) { return false; }
+	elem.clear();
+	ss >> elem;
+	if (ss.eof() == true) { return false; }
+	this->_path = elem;
+	elem.clear();
+	ss >> elem;
+	if (ss.eof() == false) { return false; }
+	if (elem.compare("{") != 0) { return false; }
+	return true;
+}
