726bf68dd3b0a2dc68347806d13979f6d38c8424
taiphyyy <ilovenoah0105@gmail.com>
2024-02-09 20:13:03 +0900

    Make main.cpp work

--- inc/main.hpp
+++ inc/main.hpp
@@ -2,8 +2,10 @@
 #define MAIN_HPP
 
 #include "Error.hpp"
-#include "HttpRequest.hpp"
+#include "Config.hpp"
 #include "Location.hpp"
 #include "env.hpp"
+#include "ServerSocket.hpp"
+#include "loop.hpp"
 
 #endif
