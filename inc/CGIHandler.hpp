3eaee36551af4dcd6944be399284ed99688b5780
taiphyyy <ilovenoah0105@gmail.com>
2024-02-20 12:04:54 +0900

    Add CGIHandler class

---
+++ inc/CGIHandler.hpp
@@ -0,0 +1,36 @@
+#ifndef CGIHANDLER_HPP
+#define CGIHANDLER_HPP
+
+#include <string>
+#include <map>
+
+#include "Request.hpp"
+
+class CGIHandler
+{
+	public:
+		enum cgiphase { CGIEXEC, CGIWRITE, CGIRECV, CGIFIN };
+
+	private:
+		std::vector<const char *> env;
+		std::string _scriptPath;
+		std::string _runtimePath;
+		bool _isActive;
+		short _revents;
+		int _wpfd;
+		int _rpfd;
+		static std::vector<bool (CGIHandler::*)(const Request &, const std::string &)> _initMetaVarSetterVec();
+		static std::vector<bool (CGIHandler::*)(const Request &, const std::string &)> _metaVarSetterVec;
+
+	public:
+		bool init(const Request &request, std::string const &scriptPath, std::string const &runtimePath);
+		bool activate();
+		bool isActive() const;
+		void setRevents(const short revents);
+		short getRevents() const;
+		int getMonitoredFd() const;
+		bool setAuthType(const Request &request, const std::string &scriptPath);
+		CGIHandler::cgiphase getCGIPhase() const;
+};
+
+#endif
