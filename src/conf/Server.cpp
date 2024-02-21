aaef7efa9c1d5558f2b41de03168b49c244d50b2
taiphyyy <ilovenoah0105@gmail.com>
2024-02-21 15:21:30 +0900

    default root and setroot changed using std::getenv("PWD")

--- src/conf/Server.cpp
+++ src/conf/Server.cpp
@@ -22,7 +22,7 @@ std::map<std::string, bool (Location::*)(std::string const &, std::fstream &)>
 	Server::_setterMap = initSetterMap();
 
 Server::Server() : AConfigurable(), _ipAddr("0.0.0.0"), _port("8000") {
-	this->_root = "./";
+	this->_root = std::getenv("PWD");
 	this->_servernames.push_back("");
 }
 
