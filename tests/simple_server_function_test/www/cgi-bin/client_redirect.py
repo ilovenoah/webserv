2fbb17a91460d56c8b2eb1f0e6e70b8bf528d505
taiphyyy <ilovenoah0105@gmail.com>
2024-02-28 17:30:37 +0900

    add new cgi test

---
+++ tests/simple_server_function_test/www/cgi-bin/client_redirect.py
@@ -0,0 +1,4 @@
+print("Status: 302 Found")
+print("Location: http://localhost/")
+print("Content-Type: text/html")
+print()  # ヘッダーの終わりを示す空行
---
+++ tests/simple_server_function_test/www/cgi-bin/invalid_local_redirect.py
@@ -0,0 +1,3 @@
+print("Location: /content/")
+print("Connection: close")
+print()  # ヘッダーの終わりを示す空行
