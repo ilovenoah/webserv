8e88c835f36dac7f3d09f335bb2918d594976456
ilovenoah <ilovenoah0105@gmail.com>
2024-03-09 15:54:57 +0900

    add duplicate header in cgi response

---
+++ tests/simple_server_function_test/www/cgi-bin/duplicate_cgi_response_header.py
@@ -0,0 +1,9 @@
+import os
+
+# ステータスとコンテントタイプのヘッダーを出力
+print("Status: 200 OK")
+print("Content-Type: text/html")
+print("X_test: test1")
+print("X_test: test2")
+print("X_test: test3")
+print()  # ヘッダーの終わりを示す空行
